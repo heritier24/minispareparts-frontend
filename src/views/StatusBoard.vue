daf7b5bf7eb7a4f07f1992b8202b3ab57f6592b0
Heritier Ganza Tamba <heritierganzatamba@gmail.com>
2025-06-29 19:08:35 +0200

    removed footter for now

--- src/views/StatusBoard.vue
+++ src/views/StatusBoard.vue
@@ -154,11 +154,11 @@
         <div class="w-full px-6 mx-auto">
           <div class="flex flex-wrap items-center -mx-3 lg:justify-between">
             <div class="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-1/2 lg:flex-none">
-              <div class="text-sm leading-normal text-center text-slate-500 lg:text-left dark:text-white/70">
+              <!-- <div class="text-sm leading-normal text-center text-slate-500 lg:text-left dark:text-white/70">
                 © {{ new Date().getFullYear() }}, made with <i class="fa fa-heart"></i> by
                 <a href="https://www.tambagroup.xyz" class="font-semibold dark:text-white" target="_blank">Tamba Group</a>
                 for Auto Repair Shop.
-              </div>
+              </div> -->
             </div>
           </div>
         </div>
